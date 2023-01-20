ebc6ca14230db9827e779038711af4c443bc07a9
CesaFajar <cieshafajar08@gmail.com>
2023-01-20 17:57:46 +0700

    update fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -17,4 +22,12 @@ const navMenu = document.querySelector("#nav-menu");
 humberger.addEventListener("click", function () {
   humberger.classList.toggle("humberger-active");
   navMenu.classList.toggle("hidden");
+});
+
+//Klik close humberger
+window.addEventListener("click", function (e) {
+  if (e.target != hamberger && e.target != navMenu) {
+    hamberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
 });
